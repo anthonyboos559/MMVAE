9c2d1c483c8d9ef9853ed8d64ae585245da54c6a
Anthony J. Boos <boosan@clipper.cm.cluster>
2024-04-01 15:53:12 -0400

    adds lit

--- Lightning_CellGene.py
+++ Lightning_CellGene.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import lightning as pl
 from lightning.pytorch.loggers import TensorBoardLogger
-from argparse import ArgumentParser
+from mmvae.data.pipes import CellCensusPipeLine
+import torchdata.dataloader2 as dl
 
 class VAE(pl.LightningModule):
     """
@@ -11,21 +13,34 @@ class VAE(pl.LightningModule):
     def __init__(self, beta: float) -> None:
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(60664, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 128),
-            nn.ReLU()
+            nn.Linear(60664, 512),
+            nn.BatchNorm1d(512),
+            nn.Dropout(0.2),
+            nn.LeakyReLU(0.1),
+            nn.Linear(512, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(0.1),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(32, 128),
-            nn.ReLU(),
-            nn.Linear(128, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 60664),
-            nn.ReLU()
+            nn.Linear(32, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(0.1),
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.1),
+            nn.Linear(512, 60664),
+            nn.BatchNorm1d(60664),
+            nn.Dropout(0.2),
+            nn.LeakyReLU(0.1)
         )
-        self.mean = nn.Linear(128, 32)
-        self.var = nn.Linear(128, 32)
+        self.mean = nn.Linear(64, 32)
+        self.var = nn.Linear(64, 32)
         self.beta = beta
         self.save_hyperparameters()
 
@@ -42,92 +57,49 @@ class VAE(pl.LightningModule):
         return x_hat, mean, var
     
     def training_step(self, batch: torch.Tensor, batch_idx: int) -> tuple[torch.Tensor]:
-        inputs, target = batch
+        inputs, _ = batch
         output, mean, logvar = self(inputs)
-        recon_loss = torch.mean(torch.nn.MSELoss(reduction='sum')(output, inputs.to_dense()))
-        KLDiv_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp()))
+        recon_loss = F.mse_loss(output, inputs.to_dense(), reduction='sum')
+        KLDiv_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
         loss = recon_loss + self.beta * KLDiv_loss
-        self.log_dict({"train/loss": loss, "train/KL loss": KLDiv_loss, "train/Recon Loss": recon_loss}, on_epoch=True, on_step=True, logger=True)
+        self.log_dict({"train/loss": loss.item() / inputs.numel(), "train/KL loss": KLDiv_loss.item() / mean.numel(), "train/Recon Loss": recon_loss.item() / output.numel()},
+                       on_epoch=True, on_step=True, logger=True, batch_size=32)
         return loss
     
     def validation_step(self, batch: torch.Tensor, batch_idx: int) -> tuple[torch.Tensor]:
-        inputs, target = batch
+        inputs, _ = batch
         output, mean, logvar = self(inputs)
-        recon_loss = torch.mean(torch.nn.MSELoss(reduction='sum')(output, inputs.to_dense()))
-        KLDiv_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp()))
+        recon_loss = F.mse_loss(output, inputs.to_dense(), reduction='sum')
+        KLDiv_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
         loss = recon_loss + self.beta * KLDiv_loss
-        self.log_dict({"valid/loss": loss, "valid/KL loss": KLDiv_loss, "valid/Recon Loss": recon_loss}, on_epoch=True, on_step=True, logger=True)
+        self.log_dict({"train/loss": loss.item() / inputs.numel(), "train/KL loss": KLDiv_loss.item() / mean.numel(), "train/Recon Loss": recon_loss.item() / output.numel()},
+                       on_epoch=True, on_step=True, logger=True, batch_size=32)
 
     def test_step(self, batch: torch.Tensor, batch_idx: int) -> tuple[torch.Tensor]:
-        inputs, target = batch
+        inputs, _ = batch
         output, mean, logvar = self(inputs)
-        recon_loss = torch.mean(torch.nn.MSELoss(reduction='sum')(output, inputs.to_dense()))
-        KLDiv_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp()))
+        recon_loss = F.mse_loss(output, inputs.to_dense(), reduction='sum')
+        KLDiv_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
         loss = recon_loss + self.beta * KLDiv_loss
-        self.log_dict({"test/loss": loss, "test/KL loss": KLDiv_loss, "test/Recon Loss": recon_loss}, on_epoch=True, on_step=True, logger=True)
+        self.log_dict({"train/loss": loss.item() / inputs.numel(), "train/KL loss": KLDiv_loss.item() / mean.numel(), "train/Recon Loss": recon_loss.item() / output.numel()},
+                       on_epoch=True, on_step=True, logger=True, batch_size=32)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters())
 
-import mmvae.data.utils as utils
-def configure_singlechunk_dataloaders(
-    data_file_path: str,
-    metadata_file_path: str,
-    train_ratio: float,
-    batch_size: int,
-    device: torch.device,
-    test_batch_size: int = None
-):
-    """
-    Splits a csr_matrix provided by data_file_path with equal length metadata_file_path by train_ratio 
-        which is a floating point between 0-1 (propertion of training data to test data). 
-    
-    If device is not None -> the entire dataset will be loaded on device at once.
-    """
-    if not test_batch_size:
-        test_batch_size = batch_size
-        
-    (train_data, train_metadata), (validation_data, validation_metadata) = utils.split_data_and_metadata(
-        data_file_path,
-        metadata_file_path,
-        train_ratio)
-    
-    from mmvae.data.datasets.CellCensusDataSet import CellCensusDataset, collate_fn
-    if device:
-        train_data = train_data.to(device)
-        validation_data = validation_data.to(device)
-        
-    train_dataset = CellCensusDataset(train_data, train_metadata)
-    test_dataset = CellCensusDataset(validation_data, validation_metadata)
-    
-    from torch.utils.data import DataLoader
-    return (
-        DataLoader(
-            train_dataset,
-            batch_size=batch_size,
-            shuffle=True,
-            collate_fn=collate_fn,
-        ),
-        DataLoader(
-            test_dataset,
-            shuffle=True,
-            batch_size=test_batch_size,
-            collate_fn=collate_fn,
-        )
-    )
-
 def main():
-    model = VAE(1)
-    logger = TensorBoardLogger(save_dir="/active/debruinz_project/tony_boos/tb_logs", version=0, name='Lightning Test')
+    pipeline = CellCensusPipeLine(directory_path="/active/debruinz_project/CellCensus_3M/", masks=["*human*.npz"], batch_size=32)
+    train_pipe, val_pipe = pipeline.random_split(weights={"train": 0.8, "test": 0.2}, total_length=3000000, seed=42)
+    train_loader = dl.DataLoader2(datapipe=train_pipe, datapipe_adapter_fn=None, reading_service=dl.MultiProcessingReadingService(num_workers=0))
+    val_loader = dl.DataLoader2(datapipe=val_pipe, datapipe_adapter_fn=None, reading_service=dl.MultiProcessingReadingService(num_workers=0))
+    # train_loader = ChunkedCellCensusDataLoader(None, directory_path="/active/debruinz_project/human_data/python_data",
+    #                                             masks=['human_chunk_[1-7][0-9]*', 'human_chunk_[1-9][.]*'], batch_size=32, num_workers=0)
+    # val_loader = ChunkedCellCensusDataLoader(None, directory_path="/active/debruinz_project/human_data/python_data",
+    #                                             masks=['human_chunk_[89][0-9]*'], batch_size=32, num_workers=0)
+    model = VAE(0.15)
+    logger = TensorBoardLogger(save_dir="/active/debruinz_project/tony_boos/tb_logs", version=0.2, name='New_Arch_Test', default_hp_metric=False)
     logger.log_hyperparams(model.hparams)
-    train_loader, val_loader = configure_singlechunk_dataloaders(
-        data_file_path="/active/debruinz_project/CellCensus_3M/3m_human_chunk_10.npz",
-        metadata_file_path="/active/debruinz_project/CellCensus_3M/3m_human_metadata_10.csv",
-        train_ratio=0.8,
-        batch_size=32,
-        device="cuda"
-    )
-    trainer = pl.Trainer(accelerator='gpu', devices=1, logger=logger, max_epochs=30)
+    trainer = pl.Trainer(accelerator='gpu', devices=1, logger=logger, max_epochs=5, enable_progress_bar=False, enable_checkpointing=False)
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
     #trainer.test(model)
 
